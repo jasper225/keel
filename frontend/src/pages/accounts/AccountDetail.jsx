221f66e5da54e4634faef4ca812df3e787bcb889
jasper225 <jaspermarkarian@gmail.com>
2026-09-03 13:40:52 -0400

    Updated initial account detail page

--- frontend/src/pages/accounts/AccountDetail.jsx
+++ frontend/src/pages/accounts/AccountDetail.jsx
@@ -1,21 +1,24 @@
-import { useEffect } from "react";
-import { useAccount } from "../../hooks/useAccounts";
-import { useAccountBalance } from "../../hooks/useAccounts";
+import { useParams } from 'react-router-dom';
+import { useAccount, useAccountBalance } from "../../hooks/useAccounts";
 
 function formatCurrency(amount, currency) {
     return new Intl.NumberFormat('en-US', { style: 'currency', currency }).format(amount);
 }
 
 export default function AccountDetail() {
-    const { data: account, isLoading, error } = useAccount();
-    if (isLoading) return <p>Loading accounts ...</p>;
-    if (error) return <p className='text-red-600'>Error loading accounts</p>;
+    const { accountId } = useParams();
+    const { data: account, isLoading, error } = useAccount(accountId);
+    const { data: balance } = useAccountBalance(accountId);
+    if (isLoading) return <p>Loading account ...</p>;
+    if (error) return <p className='text-red-600'>Error loading account</p>;
     return (
         <div>
             <h1 className='text-xl font-semibold text-gray-900'>{account.name}</h1>
-            <h2>Account Type: {account.type}</h2>
-            <h2>Account Currency: {account.currency}</h2>
-            <h2>Account Balance: {account.balance}</h2>
+            <h2 className='font-semibold text-gray-900'>Account Type: {account.type}</h2>
+            <h2 className='font-semibold text-gray-900'>Account Currency: {account.currency}</h2>
+            <h2 className='font-semibold text-gray-900'>
+            Account Balance: {balance !== undefined ? formatCurrency(balance, account.currency) : '...'}
+            </h2>
         </div>
     )
 }
