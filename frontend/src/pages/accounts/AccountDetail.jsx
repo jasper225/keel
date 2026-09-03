32c7c033995f66eaddce262777dee2f10b0fce5e
Jasper <jaspermarkarian@gmail.com>
2026-09-03 10:46:06 -0400

    Account detail page prototype

--- frontend/src/pages/accounts/AccountDetail.jsx
+++ frontend/src/pages/accounts/AccountDetail.jsx
@@ -1,9 +1,21 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
+import { useAccount } from "../../hooks/useAccounts";
+import { useAccountBalance } from "../../hooks/useAccounts";
+
+function formatCurrency(amount, currency) {
+    return new Intl.NumberFormat('en-US', { style: 'currency', currency }).format(amount);
+}
 
 export default function AccountDetail() {
+    const { data: account, isLoading, error } = useAccount();
+    if (isLoading) return <p>Loading accounts ...</p>;
+    if (error) return <p className='text-red-600'>Error loading accounts</p>;
     return (
-        <div className="account-details">
-
+        <div>
+            <h1 className='text-xl font-semibold text-gray-900'>{account.name}</h1>
+            <h2>Account Type: {account.type}</h2>
+            <h2>Account Currency: {account.currency}</h2>
+            <h2>Account Balance: {account.balance}</h2>
         </div>
     )
 }
