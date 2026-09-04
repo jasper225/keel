e612f99864eb3f05564c76169a4680af0306644d
Jasper <jaspermarkarian@gmail.com>
2026-09-04 18:05:43 -0400

    minor transaction detail page tweaks

--- frontend/src/pages/transactions/TransactionDetail.jsx
+++ frontend/src/pages/transactions/TransactionDetail.jsx
@@ -3,15 +3,15 @@ import { useTransaction } from "../../hooks/useTransactions";
 
 export default function TransactionDetail() {
     const { transactionId } = useParams();
-    const { data: account, isLoading, error } = useTransaction(transactionId);
-        if (isLoading) return <p>Transaction ...</p>;
-        if (error) return <p className='text-red-600'>Error loading transaction</p>;
+    const { data: transaction, isLoading, error } = useTransaction(transactionId);
+    if (isLoading) return <p>Loading transaction ...</p>;
+    if (error) return <p className='text-red-600'>Error loading transaction</p>;
         return (
             <div>
                 <h1 className='text-xl font-semibold text-gray-900'>Transaction Details</h1>
-                <h2 className='font-semibold text-gray-900'>Account: {account.type}</h2>
-                <h2 className='font-semibold text-gray-900'>Category: {account.currency}</h2>
-                <h2 className='font-semibold text-gray-900'>Amount: {account.currency}
+                <h2 className='font-semibold text-gray-900'>Account: {transaction.account}</h2>
+                <h2 className='font-semibold text-gray-900'>Category: {transaction.category}</h2>
+                <h2 className='font-semibold text-gray-900'>Amount: {transaction.amount}
                 </h2>
             </div>
         )
